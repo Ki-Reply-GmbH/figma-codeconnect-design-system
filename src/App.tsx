f2073eb949204d756016b47315801779617a5d99
Maurits De Roover <maurits.de.roover@outlook.com>
2026-03-02 12:31:07 +0100

    initial trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Tag } from './components/Tag/Tag'
 import TextStyles from './components/TextStyles'
 
 function App() {
-  return <TextStyles />
+  return (
+    <div className="flex items-center gap-4 p-8">
+      <Tag size="S">Tag S</Tag>
+      <Tag size="XS">Tag XS</Tag>
+    </div>
+  )
 }
 
 export default App
